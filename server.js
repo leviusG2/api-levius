0f80e62ad08e6616a175ecb8787f17c203b250ec
leviusG2 <76997367+leviusG2@users.noreply.github.com>
2022-02-28 15:18:02 +0700

    update port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 dotenv.config();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 mongoose.connect('mongodb+srv://Levius810:TVgdomSNt9OXKfC2@leviushrm.lyiej.mongodb.net/CRUD?retryWrites=true&w=majority', (err) => {
     if (err) throw err
